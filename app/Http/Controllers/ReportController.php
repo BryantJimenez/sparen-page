30b3b57386418890abf647600246c6eaebde5ca5
unknown <laurazzrangel760@gmail.com>
2020-05-21 11:27:41 -0400

    report storeEnglish

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -47,85 +47,83 @@ class ReportController extends Controller
     public function store(ReportStoreSpanishRequest $request)
     {
 
+        $count=Report::where('title_spanish', $request->title_spanish)->count();
+        $slug=Str::slug($request->title_spanish, '-');
+        if ($count>0) {
+            $slug=$slug."-".$count;
+            $num=0;
+            while (true) {
+                $count2 = Report::where('slug', $slug)->count();
+                if ($count2>0) {
+                    $slug=$slug."-".$num;
+                    $num++;
+                } else {
+                    $data=array('title_spanish' => request('title_spanish'), 'slug' => $slug, 'pdf_spanish' => $request->pdf_spanish, 'description_spanish' => request('description_spanish'), 
+                        'user_id' => Auth::user()->id);
+                    break;
+                }
+            }
 
-        // Validación para que no se repita el slug (si el pdf es en español)
-
-        // if ($request->has('title_spanish')) {
-
-        //     $count=Report::where('title_spanish', $request->title_spanish)->count();
-        //     $slug=Str::slug($request->title_spanish, '-');
-        //     if ($count>0) {
-        //         $slug=$slug."-".$count;
-        //         $num=0;
-        //         while (true) {
-        //             $count2 = Report::where('slug', $slug)->count();
-        //             if ($count2>0) {
-        //                 $slug=$slug."-".$num;
-        //                 $num++;
-        //             } else {
-        //                 $data=array('title_spanish' => request('title_spanish'), 'slug' => $slug, 'pdf_spanish' => $request->pdf_spanish, 'description_spanish' => request('description_spanish'), 
-        //                     'user_id' => Auth::user()->id);
-        //                 break;
-        //             }
-        //         }
-        //     }
-
-        //       if ($request->hasFile('pdf_spanish')) {
-        //         $file = $request->file('pdf_spanish');
-        //         $pdf = time()."_".$file->getClientOriginalName();
-        //         $file->move(public_path().'/web/images/reports/', $pdf);
-        //         $data['pdf_spanish'] = $pdf;
-
-        //     }
-
-        //      // Validación para que no se repita el slug (si el pdf es en inglés)
-
-        // } else{
-
-        //     $count=Report::where('title_english', request('title_english'))->count();
-
-        //     $slug=Str::slug(request('title_english'), '-');
-        //     if ($count>0) {
-        //         $slug=$slug."-".$count;
-        //     }
-
-        //     $num=0;
-        //     while (true) {
-        //         $count2 = Report::where('slug', $slug)->count();
-        //         if ($count2>0) {
-        //             $slug=$slug."-".$num;
-        //             $num++;
-        //         } else {
-        //             $data=array('title_english' => request('title_english'), 'slug' => $slug, 'pdf_english' => request('pdf_english'), 'description_english' => request('description_english'), 'user_id' => Auth::user()->id);
-        //             break;
-        //         }
-        //     }
+        }
 
 
-        //     if ($request->hasFile('pdf_english')) {
-        //         $file = $request->file('pdf_english');
-        //         $pdf = time()."_".$file->getClientOriginalName();
-        //         $file->move(public_path().'/web/images/reports/', $pdf);
-        //         $data['pdf_english'] = $pdf;
+        $file = $request->file('pdf_spanish');
+        $pdf = time()."_".$file->getClientOriginalName();
+        $file->move(public_path().'/web/images/reports/', $pdf);
+        $data['pdf_spanish'] = $pdf;
 
-        //     }
+        $report=Report::create($data);
 
-
-        // }
-
-        
-        // $report=Report::create($data);
-
-        // if ($report) {
-        //     return redirect()->route('informe.index')->with(['type' => 'success', 'title' => 'Registro exitoso', 'msg' => 'El informe ha sido registrado exitosamente.']);
-        // } else {
-        //     return redirect()->route('informe.index')->with(['type' => 'error', 'title' => 'Registro fallido', 'msg' => 'Ha ocurrido un error durante el proceso, intentelo nuevamente.']);
-        // } 
-    }
-
-    public function storeEnlgish(ReportStoreEnglishRequest $request) {
+        if ($report) {
+            return redirect()->route('informe.index')->with(['type' => 'success', 'title' => 'Registro exitoso', 'msg' => 'El informe ha sido registrado exitosamente.']);
+        } else {
+            return redirect()->route('informe.index')->with(['type' => 'error', 'title' => 'Registro fallido', 'msg' => 'Ha ocurrido un error durante el proceso, intentelo nuevamente.']);
+        } 
 
     }
+
+    public function storeEnglish(ReportStoreEnglishRequest $request)
+    {
+
+        $count=Report::where('title_english', $request->title_english)->count();
+        $slug=Str::slug($request->title_english, '-');
+        if ($count>0) {
+            $slug=$slug."-".$count;
+            $num=0;
+            while (true) {
+                $count2 = Report::where('slug', $slug)->count();
+                if ($count2>0) {
+                    $slug=$slug."-".$num;
+                    $num++;
+                } else {
+                    $data=array('title_english' => request('title_english'), 'slug' => $slug, 'pdf_english' => $request->pdf_english, 'description_english' => request('description_english'), 
+                        'user_id' => Auth::user()->id);
+                    break;
+                }
+            }
+
+        }
+
+
+        $file = $request->file('pdf_english');
+        $pdf = time()."_".$file->getClientOriginalName();
+        $file->move(public_path().'/web/images/reports/', $pdf);
+        $data['pdf_english'] = $pdf;
+
+        $report=Report::create($data);
+
+        if ($report) {
+            return redirect()->route('informe.index')->with(['type' => 'success', 'title' => 'Registro exitoso', 'msg' => 'El informe ha sido registrado exitosamente.']);
+        } else {
+            return redirect()->route('informe.index')->with(['type' => 'error', 'title' => 'Registro fallido', 'msg' => 'Ha ocurrido un error durante el proceso, intentelo nuevamente.']);
+        } 
+
+    }
+
+
+
+
+
 
     /**
      * Display the specified resource.
